448ec196f4f46e4cacaeebd7b336cde6132e95f7
cobrianz <briancheruiyot.h@gmail.com>
2023-12-14 21:07:30 +0300

    first commit

---
+++ Eduka Admin Panel/README.md
@@ -0,0 +1,2 @@
+# Admin-panel
+A simple Education website backend Admin panel using HTML,CSS and javascript
---
+++ Eduka Admin Panel/orders.js
@@ -0,0 +1,107 @@
+const Orders = [
+    {
+      orderName: 'Hisense 16" TV',
+      orderNumber: "85631",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 19,999.00"
+    },
+    {
+      orderName: "GLD model 608pro",
+      orderNumber: "85632",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 10,000.00"
+    },
+    {
+      orderName: "Pro cooker",
+      orderNumber: "85633",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 3,499.00"
+    },
+    {
+      orderName: "Infinix Smart 5",
+      orderNumber: "85634",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 12,999.00"
+    },
+    {
+      orderName: "Water dispensor",
+      orderNumber: "85635",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 8,000.00"
+    },
+    {
+      orderName: "Jordan 9",
+      orderNumber: "85636",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 4,499.00"
+    },
+    {
+      orderName: "queen bed complete set",
+      orderNumber: "85637",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 35,000.00"
+    },
+    {
+      orderName: "Maize floor 12''",
+      orderNumber: "85638",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 2,500.00"
+    },
+    {
+      orderName: "Cooking Pan 6''",
+      orderNumber: "85639",
+      paymentStatus: "Refundend",
+      shipping: "Declined",
+      price: "Ksh. 4,999.00"
+    },
+    {
+      orderName: "Asus laptop",
+      orderNumber: "85640",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 98,000.00"
+    },
+    {
+      orderName: "Sofa seat",
+      orderNumber: "85631",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 100,000.00"
+    },
+    {
+      orderName: "Wheat floor",
+      orderNumber: "85632",
+      paymentStatus: "Refundend",
+      shipping: "Declined",
+      price: "Ksh. 199.00"
+    },
+    {
+      orderName: "Cooking oil 1lr",
+      orderNumber: "85633",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 999.00"
+    },
+    {
+      orderName: "Mountain Bike",
+      orderNumber: "85634",
+      paymentStatus: "payment received",
+      shipping: "Delivered",
+      price: "Ksh. 29,999.00"
+    },
+    {
+      orderName: "Balenciaga T-shirt",
+      orderNumber: "85635",
+      paymentStatus: "Refundend",
+      shipping: "Declined",
+      price: "Ksh. 1999.00"
+    }
+  ];
