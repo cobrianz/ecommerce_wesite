5672d4a2345ac1f9c039566b1c558510689ab51f
cobrianz <briancheruiyot.h@gmail.com>
2024-01-14 23:35:22 +0300

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
   /* form */
 
-
+  const signup = document.querySelector('.form__signup');
+  const signin = document.querySelector('.form__login');
+  const signupBtn = document.querySelectorAll('.button__light');
+  const signinBtn = document.querySelectorAll('.button__light');
 
 
 function toggleForm() {
