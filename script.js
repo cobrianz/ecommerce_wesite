136a0c3213f5da37b6b12d3d84367d29fcff10e2
cobrianz <briancheruiyot.h@gmail.com>
2024-01-15 23:33:16 +0300

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,17 @@
   const signupBtn = document.querySelectorAll('.button__light');
   const signinBtn = document.querySelectorAll('.button__light');
 
+
+function toggleForm() {
+  signup.style.right = "0%";
+  signin.style.left = "-100%";
+}
+function toggleform() {
+  signin.style.left = "50%";
+  signup.style.right = "-100%";
+}
+
+function redirect() {
+  Window.location.href = "index.html";
+}
+
